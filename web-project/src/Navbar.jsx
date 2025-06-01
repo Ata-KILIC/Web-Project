06199edd9a211820b8120277791fdf0e437e827c
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2025-06-02 01:48:42 +0300

    update

---
+++ web-project/src/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>new Web Project</h1>
+      <div className="links">
+        <a href="/">Home</a>
+
+        <a href="/create"> new blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
