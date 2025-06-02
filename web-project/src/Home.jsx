4fea5b2d7c1c9dabd431b09bc062a5bed22bd575
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2025-06-03 00:21:42 +0300

    blogging

--- web-project/src/Home.jsx
+++ web-project/src/Home.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
-
-        <div className="home">
-            <h2>Home Page</h2>
-        </div>
 
 
+  const [blogs, setBlogs] = useState([
+    { title: "Blog1", body: "lorem ipsum dolor sit amet", author: "Author1", id: 1 },
+    { title: "Blog2", body: "lorem ipsum dolor sit amet", author: "Author2", id: 2 },
+    { title: "Blog3", body: "lorem ipsum dolor sit amet", author: "Author3", id: 3 },
 
+  ]);
 
-     );
-}
- 
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+      <button onClick = {handleClick}> Click me </button> 
+    </div>
+  );
+};
+
 export default Home;
--- web-project/src/Navbar.jsx
+++ web-project/src/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>new Web Project</h1>
+      <h1>Project Thoth</h1>
       <div className="links">
         <a href="/">Home</a>
 
